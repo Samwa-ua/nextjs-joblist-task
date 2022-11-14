686893b1fd1dcd6dee65d2a5f0b2fa7c4ff1e8e0
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-11-14 16:03:33 +0200

    fix: Add correct types for dateCount function

--- utils/countDate.ts
+++ utils/countDate.ts
@@ -1,7 +1,7 @@
-const currentDate: any = new Date();
-const countDate = (startDate: any) => {
-    startDate = new Date(startDate);
-    const difference = (currentDate - startDate) / (1000 * 3600 * 24);
+const currentDate: Date = new Date();
+const countDate = (dateFromApi: string) => {
+    const startDate = new Date(dateFromApi);
+    const difference = (Number(currentDate) - Number(startDate)) / (1000 * 3600 * 24);
     return `Posted ${Math.floor(difference)} days ago`;
 };
 
