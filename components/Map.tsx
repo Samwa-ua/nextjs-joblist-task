fe5241180a2c450492b7a088c078ec4e66abbf1c
Andrii Skokovskyi <askokovskiy@gmail.com>
2022-11-17 09:34:22 +0200

    fix: Clear redundant code for Map component

--- components/Map.tsx
+++ components/Map.tsx
@@ -37,32 +37,14 @@ const Map = (props: any) => {
     googleMapsApiKey: APIKEY ? APIKEY : "",
   });
 
-  const [map, setMap] = React.useState(null);
-
-  const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    setMap(map);
-  }, []);
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null);
-  }, []);
-
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
+      zoom={15}
       options={defaultOptions}
     >
       <CustomMarker center={center} />
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
     </GoogleMap>
   ) : (
     <></>
